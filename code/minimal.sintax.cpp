15f631c20fbde6f1b6d0165226c85e6f038f2ca0
HeckRodSav <hrs.heitor@hotmail.com.br>
2017-09-03 19:34:39 -0300

    0.2 - Sintax
    The syntax chapter is ready.
    An ASCII table has been added to the appendices.
    More flowcharts.
    Started the bibliography file.

---
+++ code/minimal.sintax.cpp
@@ -0,0 +1,12 @@
+Int main()     //Esta linha nao sera aceita por causa da letra maiuscula
+{              //O bloco principal inicia aqui
+	return 0;  //Este comando tem ; no final
+}              //O bloco principa acaba aqui
+
+/*
+Aqui podemos escrever qualquer coisa maior, normalmente este tipo de comentario contem nome do programa, do programador, etc
+int main()
+{
+	return 0;
+}
+*/
